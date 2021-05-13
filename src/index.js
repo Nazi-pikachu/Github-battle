18633858f35f8bdbfb02508afceb2fb55f3f55a5
Rishabh Jain <jain.rishabhche@gmail.com>
2021-05-13 16:14:32 +0530

    added basename in the BrowseRouter

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Github-battle">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
